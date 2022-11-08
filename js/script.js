25969fa29e41d1f3d4564d6124a6b597acf476d8
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-11-08 14:31:54 +0100

    end add button  add email & refresh email + bonus

--- js/script.js
+++ js/script.js
@@ -14,11 +14,20 @@ creaArrayRandom(array){
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
         array.push(element.data.response)
     })
+},
+pulisciArray(){
+    this.a.splice(0);
+    for (let i = 0; i < 10; i++) {
+        this.creaArrayRandom(this.a);
+        
+}
+  
 }
 },
 mounted(){
-    for (let i = 0; i < 10; i++) {
+     for (let i = 0; i < 10; i++) {
         this.creaArrayRandom(this.a);
+        
 }
 
 }}).mount('#app')
